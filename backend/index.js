1b54ec923940fb262343937665d10f6cb9f0e713
santi <l.santi@estudante.ifro.edu.br>
2026-08-07 20:04:36 -0400

    atualizando front

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 const port = 3001;
+
+// Permite requisições de qualquer origem
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
@@ -9,7 +14,7 @@ app.get('/', (req, res) => {
 app.get('/2', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel 2!' });
 });
- 
-app.listen(port, (e) => {   
-  console.log(`Example app listening on port ${port}`);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,28 +1,31 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 const elemento = document.getElementById('app') as HTMLElement;
+const elemento1 = document.getElementById('res') as HTMLElement;
 
-
-const ulr = process.env.API_URL!
-
+// Use import.meta.env em vez de process.env
+const url = import.meta.env.VITE_API_URL;
 
 async function fetchData() {
   try {
-   
-    const response = await fetch(ulr);
-    
+    const response = await fetch(url, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
     if (!response.ok) {
+      console.log("Erro na requisição:", response.status, response.statusText);
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    
 
-    elemento.innerHTML = `<h1>Response status: ${response.status}</h1>`;
-    const data = await response.json();
+    const text = await response.text();
+    console.log('Resposta bruta da API:', text);
+
+    const data = JSON.parse(text);
+    elemento.innerHTML = `<h1>resposta da api: ${response.status}</h1>`;
+    elemento1.innerHTML = `<h1>resposta da api: ${data.message}</h1>`;
     console.log('Success:', data);
 
   } catch (error) {
-    
     console.error('Fetch error:', error);
   }
 }
